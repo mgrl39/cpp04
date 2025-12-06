73f9a0d3d6fa7e1e7013c8f806c9dd97ca1dcda8
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-12-06 22:45:57 +0100

    add information Dog Cat

--- ex00/Cat.hpp
+++ ex00/Cat.hpp
@@ -6,8 +6,24 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 12:35:24 by meghribe          #+#    #+#             */
-/*   Updated: 2025/12/05 12:35:26 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/12/06 22:29:42 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CAT_HPP
+# define CAT_HPP
 
+#include "Animal.hpp"
+#include <string>
+
+class Cat: public Animal
+{
+	public:
+		Cat();
+		~Cat();
+		Cat(const Cat &rhs);
+		Cat &operator=(const Cat &rhs);
+		override void makeSound();
+};
+
+#endif
--- ex00/Dog.hpp
+++ ex00/Dog.hpp
@@ -6,8 +6,24 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 12:35:01 by meghribe          #+#    #+#             */
-/*   Updated: 2025/12/05 12:35:02 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/12/06 22:45:25 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef DOG_HPP
+# define DOG_HPP
 
+#include "Animal.hpp"
+#include <string>
+
+class Dog: public Animal
+{
+	public:
+		Dog();
+		~Dog();
+		Dog(const Dog &rhs);
+		Dog &operator=(const Dog &rhs);
+		override void makeSound();
+};
+
+#endif
