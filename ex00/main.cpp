60d2d916a59ff4e1210c2b13c56d24009e4317cc
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-12-15 12:36:50 +0100

    improved main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 12:34:05 by meghribe          #+#    #+#             */
-/*   Updated: 2025/12/07 00:52:43 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/12/15 12:34:06 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,64 +17,149 @@
 #include "Cat.hpp"
 #include <iostream>
 
-void	testConstructors(void)
+# define RED "\033[31m"
+# define CYAN "\033[36m"
+# define RES "\033[0m"
+# define YELLOW "\033[33m"
+# define GREEN "\033[32m"
+
+void	writeText(std::string color, std::string text)
 {
-	{
-		Animal	animal;
-		animal.makeSound();
-	}
+	std::string	var;
 
-	std::cout << "========================" << std::endl;
-	{
-		Dog 	dog;
-		dog.makeSound();
-	}
-
-	std::cout << "========================" << std::endl;
-	{
-		Dog	first;
-		Dog	second(first);
-	}
-
-	std::cout << "========================" << std::endl;
-	{
-		Dog	first;
-		Dog	second;
-
-		second = first;
-	}
+	if (color == "red")
+		var = RED;
+	else if (color == "cyan")
+		var = CYAN;
+	else if (color == "yellow")
+		var = YELLOW;
+	else
+		var = GREEN;
+	std::cout << var << text << RES << std::endl;
 
 }
 
-void	mandatoryMain()
+void	testAnimalConstructors()
+{
+	writeText("", "Default Constructor ~~~~~~~~~~~~~~~~~~~~");
+	{
+		Animal animal;
+		writeText("yellow", animal.getType());
+	}
+	writeText("", "Copy Constructor ~~~~~~~~~~~~~~~~~~~~");
+	{
+		Animal first;
+		Animal second(first);
+		writeText("yellow", first.getType());
+		writeText("yellow", second.getType());
+	}
+	writeText("", "Copy assignment operator ~~~~~~~~~~~~~~~~~~~~");
+	{
+		Animal first;
+		Animal second;
+
+		second = first;
+		writeText("yellow", first.getType());
+		writeText("yellow", second.getType());
+	}
+}
+
+void	testCatConstructors()
+{
+	writeText("", "Default Constructor ~~~~~~~~~~~~~~~~~~~~");
+	{
+		Cat cat;
+		writeText("yellow", cat.getType());
+	}
+	writeText("", "Copy Constructor ~~~~~~~~~~~~~~~~~~~~");
+	{
+		Cat first;
+		Cat second(first);
+		writeText("yellow", first.getType());
+		writeText("yellow", second.getType());
+	}
+	writeText("", "Copy assignment operator ~~~~~~~~~~~~~~~~~~~~");
+	{
+		Cat first;
+		Cat second;
+
+		second = first;
+		writeText("yellow", first.getType());
+		writeText("yellow", second.getType());
+	}
+}
+
+void	testDogConstructors()
+{
+	writeText("", "Default Constructor ~~~~~~~~~~~~~~~~~~~~");
+	{
+		Dog dog;
+		writeText("yellow", dog.getType());
+	}
+	writeText("", "Copy Constructor ~~~~~~~~~~~~~~~~~~~~");
+	{
+		Dog first;
+		Dog second(first);
+		writeText("yellow", first.getType());
+		writeText("yellow", second.getType());
+	}
+	writeText("", "Copy assignment operator ~~~~~~~~~~~~~~~~~~~~");
+	{
+		Dog first;
+		Dog second;
+
+		second = first;
+		writeText("yellow", first.getType());
+		writeText("yellow", second.getType());
+	}
+}
+
+/*
+ * The only thing changed here from the original main of 
+ * the subject is the colors
+ * and the deletes
+ */
+void	mainFromSubject()
 {
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
+	std::cout << YELLOW << std::endl;
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
 	i->makeSound(); //will output the cat sound!
 	j->makeSound();
+	std::cout << RES << std::endl;
 	meta->makeSound();
 	delete meta;
 	delete j;
 	delete i;
 }
 
-void	mandatoryWrong()
+void	wrongAnimalImplementation()
 {
 	const WrongAnimal* meta = new WrongAnimal();
 	const WrongAnimal* j = new WrongCat();
-	std::cout << j->getType() << " " << std::endl;
+	//std::cout << j->getType() << " " << std::endl;
+	writeText("yellow", j->getType());
+	std::cout << YELLOW << std::endl;
 	meta->makeSound();
 	j->makeSound();
+	std::cout << RES << std::endl;
 	delete meta;
 	delete j;
 }
 
 int	main(void)
 {
-	// testConstructors();
-	mandatoryMain();
-	mandatoryWrong();
+	writeText("cyan", "========== testing canonical ortodox form ==========");
+	/*
+	testAnimalConstructors();
+	testCatConstructors();
+	testDogConstructors();
+	*/
+	writeText("cyan", "=========== main from subject =========");
+	mainFromSubject();
+	writeText("cyan", "=========== wronganimal wrongcat main implementation ===========");
+	wrongAnimalImplementation();
 }
