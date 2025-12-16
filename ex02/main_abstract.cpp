89164d9548d322bf39ddcf1dfd98bf2079dfe9be
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-12-16 23:14:51 +0100

    fix big problem in dog.cpp

--- ex02/main_abstract.cpp
+++ ex02/main_abstract.cpp
@@ -6,7 +6,7 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 21:50:20 by meghribe          #+#    #+#             */
-/*   Updated: 2025/12/16 22:20:43 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/12/16 22:55:22 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,17 @@
 #include "brain.h"
 #include "abstract.h"
 
+// The message printed is similar to the error you get if you quit the '//' in Animal a;
+void	testAbstractClassInstance()
+{
+	writeText("", "variable type 'Animal' is an abstract class");
+	// Animal a;
+}
+
 int	main(void)
 {
+	testCatConstructors();
+	testDogConstructors();
+	// testAbstractClassInstance();
 	return (0);
 }
