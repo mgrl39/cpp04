59faac2287f679d71bb8dc0dd0a5b665cd5ec575
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-12-16 21:49:46 +0100

    improve code adding .h brain.h

---
+++ ex01/brain.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   brain.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/16 21:46:27 by meghribe          #+#    #+#             */
+/*   Updated: 2025/12/16 21:47:08 by meghribe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BRAIN_H
+# define BRAIN_H
+
+# include "Brain.hpp"
+
+void	testBrainConstructors();
+void	testDeepCopyBrain();
+
+#endif
